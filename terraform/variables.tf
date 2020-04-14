0d2ef4b1cfd4ba852ca434fe13356984031acbd1
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2020-04-14 11:53:57 +0500

    basic tasks

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Private key path"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
