87efa14251cdcacdcad937484e109bb743f96a45
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2020-04-14 21:50:45 +0500

    check erros

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "google" {
   region = var.region
 }
 resource "google_compute_project_metadata_item" "default" {
-  key   = "ssh-keys"
+  key = "ssh-keys"
   value = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}"
 }
 
