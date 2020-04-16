cca3b2318aeb96bbe27b2c9bc65c30ee77331889
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2020-04-16 12:40:40 +0500

    modify

--- terraform/app.tf
+++ terraform/app.tf
@@ -6,16 +6,20 @@ resource "google_compute_instance" "app" {
   boot_disk {
     initialize_params { image = var.app_disk_image }
   }
+
   network_interface {
     network = "default"
     access_config {
       nat_ip = google_compute_address.app_ip.address
     }
   }
-  metadata {
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
+
+ # metadata {
+ #   ssh-keys = "appuser:${file(var.public_key_path)}"
+ # }
 }
+
+
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
 }
--- terraform/db.tf
+++ terraform/db.tf
@@ -10,11 +10,11 @@ resource "google_compute_instance" "db" {
   }
   network_interface {
     network = "default"
-    access_config = {}
+    #access_config = {}
   }
-  metadata {
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
+  #metadata {
+  # ssh-keys = "appuser:${file(var.public_key_path)}"
+  #}
  }
 resource "google_compute_firewall" "firewall_mongo" {
   name = "allow-mongo-default"
